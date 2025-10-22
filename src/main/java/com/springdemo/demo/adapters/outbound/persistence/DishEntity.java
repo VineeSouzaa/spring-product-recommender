454c709b87da19eef607f95509db444f87538104
Vinícius Souza <vine.vsb3@gmail.com>
2025-10-22 00:16:13 -0300

    fix(dish): Add UUID generate responsability to infra layer

--- src/main/java/com/springdemo/demo/adapters/outbound/persistence/DishEntity.java
+++ src/main/java/com/springdemo/demo/adapters/outbound/persistence/DishEntity.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class DishEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+//    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, length = 100)
